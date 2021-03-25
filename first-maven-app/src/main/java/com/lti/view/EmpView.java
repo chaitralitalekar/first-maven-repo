61759ed42b44f657ea5f2516ea26fa4749f47adf
chaitralitalekar <talekarchaitrali@gmail.com>
2021-03-25 12:44:02 +0530

    name updated

--- first-maven-app/src/main/java/com/lti/view/EmpView.java
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -7,7 +7,7 @@ public class EmpView {
 	public static void main(String[] args) {
 		Employee emp=new Employee();
 		emp.setEmpId(1001);
-		emp.setEmpName("John");
+		emp.setEmpName("Jack");
 		
 		System.out.println("Id: "+emp.getEmpId());
 		System.out.println("Name: "+emp.getEmpName());
